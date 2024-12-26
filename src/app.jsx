ce6724f982b5710896b4f301316b2401fdd37216
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-12-26 14:26:50 +0300

    create a loginForm with react

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { LoginForm } from './components/login-form/loginForm'
+
+export const App = () => {
+  return (
+    <div>
+      <LoginForm/>
+
+    </div>
+  )
+}
